2fe1314a5e8eb80ed0aa2e8cd9bc2596a52b2cc0
fitriaulianisa <161029948+fitriaulianisa@users.noreply.github.com>
2024-11-09 12:08:25 +0700

    vidpart1 selesai

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -9,7 +9,136 @@ class DetailScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          //DETAIL HEADER
+          //Image Utama
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              // Tombol back custom
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+              
+                  ),
+                  child: IconButton(
+                    onPressed: () {}, 
+                    icon: const Icon(Icons.arrow_back)
+                    ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //Info Atas
+                const SizedBox(
+                  height: 16,
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      candi.name,
+                      style: 
+                          const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      onPressed: () {}, 
+                      icon: const Icon(Icons.favorite_border))
+                  ],
+                ),
+                //Info Tengah
+                const SizedBox(
+                  height: 16,
+                  ),
+                Row(children: [
+                  const Icon(
+                    Icons.place, 
+                    color: Colors.red,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const SizedBox(
+                    width: 70,
+                    child: Text(
+                      'Lokasi',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(': ${candi.location}')
+                ],
+              ),
+              Row(children: [
+                  const Icon(
+                    Icons.calendar_month, 
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const SizedBox(
+                    width: 70,
+                    child: Text(
+                      'Dibangun',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(': ${candi.built}')
+                ],
+              ),
+              Row(children: [
+                  const Icon(
+                    Icons.house, 
+                    color: Colors.green,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const SizedBox(
+                    width: 70,
+                    child: Text(
+                      'Tipe',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(': ${candi.type}')
+                ],
+              ),
+              // pemisah
+              const SizedBox(
+                height: 16,
+              ),
+              Divider(
+                color: Colors.deepPurple.shade100,
+              ),
+              const SizedBox(
+                height: 16,
+              )
+
+
+              //Info Bawah
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
