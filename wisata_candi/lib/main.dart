d91257b06440c2d9de9dc693c45179aca5357d93
fitriaulianisa <161029948+fitriaulianisa@users.noreply.github.com>
2024-11-12 17:14:59 +0700

    part4

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
-
+import 'package:wisata_candi/screens/signin_screen.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -13,13 +13,28 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisaa Candi',
+      title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
         useMaterial3: true,
       ),
       // home: DetailScreen(candi: candiList[0],),
-      home: ProfileScreen(),
+      // home: ProfileScreen(),
+
+      home: SignInScreen(),
     );
   }
 }
