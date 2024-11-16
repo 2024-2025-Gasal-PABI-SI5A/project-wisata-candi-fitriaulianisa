5d05c51729ab332025866877fbac885af3b45743
fitriaulianisa <aulianisafitri434@gmail.com>
2024-11-16 10:54:37 +0700

    signupscreen

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/signin_screen.dart';
+import 'package:wisata_candi/screens/signup_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -32,9 +33,10 @@ class MainApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: DetailScreen(candi: candiList[0],),
-      home: ProfileScreen(),
+      // home: ProfileScreen(),
 
       // home: SignInScreen(),
+      home: SignupScreen(),
     );
   }
 }
